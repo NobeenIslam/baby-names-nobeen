54cee9bdc644112e88e36c50c83ea17a10de267c
Nobeen <n.islam.1@outlook.com>
2022-03-15 15:16:14 +0000

    Made filter function case in-sensitive

--- src/utils/filterNames.ts
+++ src/utils/filterNames.ts
@@ -1,7 +1,7 @@
 import babyProps from "../components/babyProps"
 
 function filterNames(data:babyProps[], input:string): babyProps[]{
-    return data.filter(data => data.name.includes(input))
+    return data.filter(data => data.name.toUpperCase().includes(input.toUpperCase()))
 
 }
 
