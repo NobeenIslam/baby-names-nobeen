90090b15ed6afa211a3f9f75524bbf76d5881086
Nobeen <n.islam.1@outlook.com>
2022-03-15 00:04:33 +0000

    Sorted by alphabetical order

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,14 +1,16 @@
 import ButtonElement from "./ButtonElement";
 import babyNamesData from "../babyNamesData";
 import babyProps from "./babyProps";
+import alphaBeticalSort from "./alphabeticalSort";
 
 function MainContent(): JSX.Element {
-  const babyNameButtons = babyNamesData.map(
-    (baby: babyProps): JSX.Element => (
-      <ButtonElement key={baby.id} name={baby.name} sex={baby.sex} />
-    )
-  );
-  return <section>{babyNameButtons}</section>;
+    const sortedBabyNames = babyNamesData.sort(alphaBeticalSort)
+    const babyNameButtons = sortedBabyNames.map(
+        (baby: babyProps): JSX.Element => (
+            <ButtonElement key={baby.id} name={baby.name} sex={baby.sex} />
+        )
+    );
+return <section>{babyNameButtons}</section>;
 }
 
 export default MainContent;
---
+++ src/components/alphabeticalSort.ts
@@ -0,0 +1,9 @@
+import babyProps from "./babyProps";
+
+function alphaBeticalSort (a:babyProps, b:babyProps):number {
+    const textA = a.name.toUpperCase();
+    const textB = b.name.toUpperCase();
+    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+}
+
+export default alphaBeticalSort
