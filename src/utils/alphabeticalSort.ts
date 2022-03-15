7065b91ee1f04afa9d61ab55010fb1a0ddf11fd1
Nobeen <n.islam.1@outlook.com>
2022-03-15 15:20:13 +0000

    Formatting

--- src/utils/alphabeticalSort.ts
+++ src/utils/alphabeticalSort.ts
@@ -1,9 +1,9 @@
-import babyProps from "../components/babyProps"
+import babyProps from "../components/babyProps";
 
-function alphaBeticalSort (a:babyProps, b:babyProps):number {
-    const textA = a.name.toUpperCase();
-    const textB = b.name.toUpperCase();
-    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+function alphaBeticalSort(a: babyProps, b: babyProps): number {
+  const textA = a.name.toUpperCase();
+  const textB = b.name.toUpperCase();
+  return textA < textB ? -1 : textA > textB ? 1 : 0;
 }
 
-export default alphaBeticalSort
+export default alphaBeticalSort;
