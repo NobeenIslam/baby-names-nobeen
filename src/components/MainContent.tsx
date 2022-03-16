44dfdcc509e53de100b36a0f363815cfb41f2edd
Nobeen <n.islam.1@outlook.com>
2022-03-16 15:47:52 +0000

    Names added to favourite list on click

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -6,34 +6,50 @@ import babyProps from "./babyProps";
 
 function MainContent(): JSX.Element {
   const [nameSearch, setSearch] = useState("");
+  const [favouriteList, setFavourite] = useState<babyProps[]>([]);
 
   const alphabeticalBabyNames: babyProps[] =
     babyNamesData.sort(alphaBeticalSort);
 
-    const filteredBabyNames: babyProps[] = filterNames(
-      alphabeticalBabyNames,
-      nameSearch
-    );
-  
+  const filteredBabyNames: babyProps[] = filterNames(
+    alphabeticalBabyNames,
+    nameSearch
+  );
+
   const babyNameButtons = filteredBabyNames.map(
-    (baby: babyProps): JSX.Element => (
-      <button key = {baby.id} className={"button "+ baby.sex}>{baby.name}</button>
+    (baby) => (
+      <button
+        key={baby.id}
+        className={"button " + baby.sex}
+        onClick={() => setFavourite([...favouriteList, baby])}
+      >
+        {baby.name}
+      </button>
     )
   );
-    
-  
 
-
+  const favouriteNameButtons = favouriteList.map(
+    (baby,index) => (
+      <button
+        key={index}
+        className={"button " + baby.sex}
+        onClick={() => console.log("hello")}
+      >
+        {baby.name}
+      </button>
+    )
+  );
 
   return (
     <>
       <input
-      value={nameSearch}
-      onChange={(event) => {
-        setSearch(event.target.value);
-      }}
-      placeholder="Search name here.."
-     />
+        value={nameSearch}
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+        placeholder="Search name here.."
+      />
+      <div>Your Favourite Names: {favouriteNameButtons}</div>
       <hr></hr>
       <div>{babyNameButtons}</div>
     </>
