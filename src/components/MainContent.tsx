355a49d0542957de162a9bd12223177947cda127
Nobeen <n.islam.1@outlook.com>
2022-03-17 18:13:09 +0000

    Fixed favButton clicks and formatted

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,26 +3,25 @@ import Data from "../babyNamesData";
 import alphaBeticalSort from "../utils/alphabeticalSort";
 import babyProps from "./babyProps";
 
-
 function MainContent(): JSX.Element {
   const [nameSearch, setSearch] = useState("");
   const [favouriteList, setFavourite] = useState<babyProps[]>([]);
- 
 
   const babyNamesData = [...Data];
   const alphabeticalNames: babyProps[] = babyNamesData.sort(alphaBeticalSort);
 
-  const filteredNames = alphabeticalNames.filter(doesSearchTermOccurInName)
-                                         .filter(doesMainOverlapWithFav)
+  const filteredNames = alphabeticalNames
+    .filter(doesSearchTermOccurInName)
+    .filter(doesMainOverlapWithFav);
 
   function doesSearchTermOccurInName(nameInfo: babyProps): boolean {
     return nameInfo.name.toLowerCase().includes(nameSearch.toLowerCase());
   }
 
-  function doesMainOverlapWithFav(nameInfo: babyProps):boolean {
-    return !favouriteList.includes(nameInfo)
+  function doesMainOverlapWithFav(nameInfo: babyProps): boolean {
+    return !favouriteList.includes(nameInfo);
   }
-  
+
   const babyNameButtons = filteredNames.map((baby, index) => (
     <button
       key={baby.id}
@@ -36,16 +35,17 @@ function MainContent(): JSX.Element {
     </button>
   ));
 
-  console.log("Favourite List Current Elements", favouriteList)
-  console.log("The main list length is now:",filteredNames.length)
+  console.log("Favourite List Current Elements", favouriteList);
+  console.log("The main list length is now:", filteredNames.length);
 
   const favouriteNameButtons = favouriteList.map((baby, index) => (
     <button
       key={index}
       className={"button " + baby.sex}
       onClick={() => {
-        const cut = favouriteList.splice(index, 1);
-        setFavourite(favouriteList);
+        const newFavouriteList = [...favouriteList];
+        newFavouriteList.splice(index, 1);
+        setFavourite(newFavouriteList);
       }}
     >
       {baby.name}
